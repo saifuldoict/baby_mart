53050a54acf8a0ca624b4936dde6742597cff20d
Md. Saiful Islam <saiful.doict@gmail.com>
2026-01-04 15:43:41 +0600

    project setup complete

---
+++ admin/src/App.tsx
@@ -0,0 +1,13 @@
+
+
+function App() {
+
+
+  return (
+    <div className="mt-10 p-20">
+      <h1 className="font-bold ">hello admin</h1>
+    </div>
+  )
+}
+
+export default App
---
+++ server/index.js
@@ -0,0 +1,19 @@
+import express from 'express'
+import dotenv from 'dotenv'
+
+
+// load env server
+dotenv.config();
+const app = express()
+const PORT = process.env.PORT || 5000
+
+app.get('/', (req, res) => {
+  res.send('Hello from baby mart server!')
+})
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+  console.log(`Client URL: ${process.env.CLIENT_URL}`);
+  console.log(`ADMIN_URL:${process.env.ADMIN_URL}`);
+  console.log(`API docs available at http://localhost:${PORT}/api/docs`)
+});
